581c21c82645e9847440ba8eace02abbdb4fbc6b
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2025-03-25 12:21:59 +0700

    Add initial project setup with environment configuration and dependencies

---
+++ lib/retriever.ts
@@ -0,0 +1,45 @@
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { config } from "dotenv";
+config();
+const apiKey = process.env.DEEPINFRA_API_KEY;
+
+
+const urls = [
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+  "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+];
+
+const docs = await Promise.all(
+  urls.map((url) => new CheerioWebBaseLoader(url).load()),
+);
+const docsList = docs.flat();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 50,
+});
+const docSplits = await textSplitter.splitDocuments(docsList);
+
+const embedding = new OpenAIEmbeddings({
+  model: "BAAI/bge-large-en-v1.5",
+  apiKey: apiKey,
+  configuration: {
+    baseURL: "https://api.deepinfra.com/v1/openai",
+  },
+  dimensions: 1024,
+});
+
+
+// Add to vectorDB
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  docSplits,
+  embedding,
+);
+
+const retriever = vectorStore.asRetriever();
+
+export default retriever;
